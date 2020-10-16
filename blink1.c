2132f705a3d765c17bb9807c6b73ab1d99484e25
priyanshagrawal29 <priyanshsinghal29@gmail.com>
2020-10-16 15:45:46 +0530

    Update blink1.c

--- blink1.c
+++ blink1.c
@@ -1,29 +1,19 @@
-int led = 13;
-int led2 = 12;
-int led3 = 11;
+#define pin 1               // using macro tp fix value of pin
 
-// the setup routine runs once when you press reset:
-void setup() {               
-  // initialize the digital pin as an output.
-  pinMode(led, OUTPUT);
-  pinMode(led2, OUTPUT);
-pinMode(led3, OUTPUT);
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(pin, OUTPUT);
+  pinMode(2, OUTPUT);
 }
 
-// the loop routine runs over and over again forever:
+// the loop function runs over and over again forever
 void loop() {
-  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);               // wait for a second
-  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);
-  digitalWrite(led2, HIGH);
-delay(1000);
-digitalWrite(led2, LOW);
-delay(1000);
-digitalWrite(led3, HIGH);
-delay(1000);
-digitalWrite(led3, LOW);
-delay(1000);// wait for a second
+  digitalWrite(pin, HIGH);   // turn the LED on
+  digitalWrite(2, LOW);
+  delay(1000);                       // wait for a second
+  digitalWrite(pin, LOW);    // turn the LED off
+  digitalWrite(2, HIGH);
+  delay(1000);                       // wait for a second
 }
-
-
